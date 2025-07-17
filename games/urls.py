1760ab06353f5eba6c16b0f1cd02ceef00cc9ba2
Annie <ajm010402@gmail.com>
2025-07-17 22:41:33 +0900

    feat: 공격하기, 반격하기 기능 추가

--- games/urls.py
+++ games/urls.py
@@ -4,4 +4,10 @@ from .views import *
 app_name = 'games'
 
 urlpatterns = [
+    path('start/', start_game, name='start'),  # 게임 생성 (공격하기)
+    path('cancel/<int:game_id>/', cancel_game, name='cancel'),  # 내가 건 게임 삭제 #예원님
+    path('counter/<int:game_id>/', counter_attack, name='counter'),  # 반격하기
+    # path('detail/<int:game_id>/', game_detail, name='detail'),  # 게임 결과 상세 #예원님
+    # path('history/', game_history, name='history'),  # 게임 전적 #예원님
+    # path('rank/', rank_view, name='rank'),  # 랭킹 보기 #지원님
 ]
